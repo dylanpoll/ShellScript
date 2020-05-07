3ff694fb16661194c3a70af6bd84aa59f6d3e81e
Dylan Poll <61894413+dylanpoll@users.noreply.github.com>
2020-05-07 04:39:47 -0400

    Update installWebIDE.sh

--- installWebIDE.sh
+++ installWebIDE.sh
@@ -1,16 +1,17 @@
-#cd
-#wget https://nodejs.org/dist/v11.15.0/node-v11.15.0-linux-armv6l.tar.xz
-#tar -xvf node-v11.15.0-linux-armv6l.tar.gz.tar.xz
-#cd node-v11.15.0-linux-armv6l
-#sudo cp -R * /usr/local/
-#git clone git://github.com/adafruit/Adafruit-WebIDE.git
-#cd Adafruit-WebIDE
-#mkdir tmp
-#npm config set tmp tmp
-#npm install
-#editor config/config.js
-#echo "to run the server as a background process type: sudo nohup node server.js &"
-#echo "to run it in this terminal type: sudo node server.js"
-#echo "the directory where you will want to run the command will be" 
-#DIRECTORY=$(pwd)
-#echo $DIRECTORY
+#!/bin/bash
+cd
+wget https://nodejs.org/dist/v11.15.0/node-v11.15.0-linux-armv6l.tar.xz
+tar -xvf node-v11.15.0-linux-armv6l.tar.gz.tar.xz
+cd node-v11.15.0-linux-armv6l
+sudo cp -R * /usr/local/
+git clone git://github.com/adafruit/Adafruit-WebIDE.git
+cd Adafruit-WebIDE
+mkdir tmp
+npm config set tmp tmp
+npm install
+editor config/config.js
+echo "to run the server as a background process type: sudo nohup node server.js &"
+echo "to run it in this terminal type: sudo node server.js"
+echo "the directory where you will want to run the command will be" 
+DIRECTORY=$(pwd)
+echo $DIRECTORY
